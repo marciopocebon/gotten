8cb82ed17b7f0fdb55803bf3856c9e45803ddfbe
hehnope <24252625+hehnope@users.noreply.github.com>
2019-06-30 13:00:56 -0700

    get Unmarshalling to work

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 func main() {
 	j_string_map := `
   {
-    'a': 5,
-    'b': 6,
-    'c': {
-      'f': 9,
-      'g': {
-        'm': 17,
-        'n': 3
+    "a": 5,
+    "b": 6,
+    "c": {
+      "f": 9,
+      "g": {
+        "m": 17,
+        "n": 3
       }
     }
   }
   `
+
+	var empty map[string]interface{}
+
+	json.Unmarshal([]byte(j_string_map), &empty)
+
+	fmt.Printf("empty: %+v\n", empty)
 
 	fmt.Println(j_string_map)
 }
